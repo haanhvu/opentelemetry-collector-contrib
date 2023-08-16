4e9bd9932232832fb63b171921ef4d752d620111
Ha Anh Vu <75315486+haanhvu@users.noreply.github.com>
2023-08-17 01:18:25 +0700

    Update exporter_traces.go

--- exporter/clickhouseexporter/exporter_traces.go
+++ exporter/clickhouseexporter/exporter_traces.go
@@ -240,7 +240,7 @@ SETTINGS index_granularity=8192, ttl_only_drop_parts = 1;
                         Links.Attributes
                         ) SETTINGS
     				async_insert = 1,
-				async_insert_threads = 100,
+				async_insert_threads = 200,
 				async_insert_max_data_size = 20000000,
     				async_insert_max_query_number = 50000,
     				async_insert_busy_timeout_ms = 500
